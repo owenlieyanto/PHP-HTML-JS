608b8d877a44371185bfc704e1deb104117fe49b
Owen Lie <62794634+owenlieyanto@users.noreply.github.com>
2021-11-21 16:49:01 +0700

    Create print.js
    working function for printing HTML element(s)

---
+++ print.js
@@ -0,0 +1,10 @@
+function printData(x) {
+        var printContents = document.getElementById(x).innerHTML;
+        var originalContents = document.body.innerHTML;
+
+        document.body.innerHTML = printContents;
+
+        window.print();
+
+        document.body.innerHTML = originalContents;
+    }
